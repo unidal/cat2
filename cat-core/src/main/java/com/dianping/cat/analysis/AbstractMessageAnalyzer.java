d64dc61dbd4c3599226825f12f154297d73bbab2
Frankie Wu <qmwu2000@gmail.com>
2016-03-20 20:15:53 +0800

    fix blocking issue when shutting down server

--- cat-core/src/main/java/com/dianping/cat/analysis/AbstractMessageAnalyzer.java
+++ cat-core/src/main/java/com/dianping/cat/analysis/AbstractMessageAnalyzer.java
@@ -1,5 +1,7 @@
 package com.dianping.cat.analysis;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.codehaus.plexus.logging.Logger;
 import org.unidal.lookup.ContainerHolder;
 import org.unidal.lookup.annotation.Inject;
@@ -30,7 +32,7 @@ public abstract class AbstractMessageAnalyzer<R> extends ContainerHolder impleme
 
 	private long m_errors = 0;
 
-	private volatile boolean m_active = true;
+	private AtomicBoolean m_active = new AtomicBoolean(true);
 
 	protected int m_index;
 
@@ -110,9 +112,7 @@ public abstract class AbstractMessageAnalyzer<R> extends ContainerHolder impleme
 	}
 
 	protected boolean isActive() {
-		synchronized (this) {
-			return m_active;
-		}
+		return m_active.get();
 	}
 
 	protected boolean isLocalMode() {
@@ -130,14 +130,11 @@ public abstract class AbstractMessageAnalyzer<R> extends ContainerHolder impleme
 
 	protected abstract void process(MessageTree tree);
 
-	public void shutdown() {
-		synchronized (this) {
-			m_active = false;
-		}
-	}
-
 	public void setIndex(int index) {
 		m_index = index;
 	}
 
+	public void shutdown() {
+		m_active.set(false);
+	}
 }
