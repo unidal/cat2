f601478b76000fd7c638c783a4328fd4d506d0ef
Frankie Wu <qmwu2000@gmail.com>
2016-04-09 20:55:09 +0800

    add MessageFilter support

--- cat-core/src/main/java/org/unidal/cat/spi/analysis/AbstractMessageAnalyzer.java
+++ cat-core/src/main/java/org/unidal/cat/spi/analysis/AbstractMessageAnalyzer.java
@@ -2,6 +2,7 @@ package org.unidal.cat.spi.analysis;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -9,17 +10,16 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.unidal.cat.spi.Report;
 import org.unidal.cat.spi.ReportManager;
 import org.unidal.cat.spi.ReportManagerManager;
-import org.unidal.lookup.annotation.Inject;
+import org.unidal.lookup.ContainerHolder;
+import org.unidal.lookup.extension.RoleHintEnabled;
 
 import com.dianping.cat.Cat;
 import com.dianping.cat.message.io.DefaultMessageQueue;
 import com.dianping.cat.message.spi.MessageQueue;
 import com.dianping.cat.message.spi.MessageTree;
 
-public abstract class AbstractMessageAnalyzer<R extends Report> implements MessageAnalyzer {
-	@Inject
-	private ReportManagerManager m_reportManagerManager;
-
+public abstract class AbstractMessageAnalyzer<R extends Report> extends ContainerHolder implements MessageAnalyzer,
+      RoleHintEnabled {
 	private String m_name;
 
 	private int m_hour;
@@ -32,16 +32,21 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 
 	private ReportManager<R> m_reportManager;
 
+	private MessageFilter m_filter;
+
 	private MessageQueue m_queue;
 
 	private CountDownLatch m_latch = new CountDownLatch(1);
 
 	private AtomicBoolean m_enabled = new AtomicBoolean(true);
 
-	public AbstractMessageAnalyzer(String name, String... dependencies) {
-		m_name = name;
+	public AbstractMessageAnalyzer(String... dependencies) {
 		m_dependencies = dependencies;
 		m_queue = new DefaultMessageQueue(getQueueSize());
+	}
+
+	@Override
+	public void configure(Map<String, String> properties) {
 	}
 
 	@Override
@@ -49,6 +54,11 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 		shutdown();
 
 		m_reportManager.doCheckpoint(new Date(TimeUnit.HOURS.toMillis(m_hour)), m_index, atEnd);
+	}
+
+	@Override
+	public void enableRoleHint(String name) {
+		m_name = name;
 	}
 
 	@Override
@@ -61,7 +71,7 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 	}
 
 	public String getName() {
-		return getClass().getSimpleName();
+		return getClass().getSimpleName() + "-" + (m_hour % 24) + "-" + m_index;
 	}
 
 	@Override
@@ -71,10 +81,6 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 
 	protected int getQueueSize() {
 		return 30000;
-	}
-
-	protected ReportManager<R> getReportManager() {
-		return m_reportManager;
 	}
 
 	protected void handleException(Throwable e) {
@@ -90,7 +96,14 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 	public void initialize(int index, int hour) throws IOException {
 		m_index = index;
 		m_hour = hour;
-		m_reportManager = m_reportManagerManager.getReportManager(m_name);
+
+		if (super.hasComponent(MessageFilter.class, m_name)) {
+			m_filter = lookup(MessageFilter.class, m_name);
+		}
+
+		ReportManagerManager rmm = lookup(ReportManagerManager.class);
+
+		m_reportManager = rmm.getReportManager(m_name);
 		m_reportManager.doInitLoad(new Date(TimeUnit.HOURS.toMillis(m_hour)), m_index);
 	}
 
@@ -103,7 +116,9 @@ public abstract class AbstractMessageAnalyzer<R extends Report> implements Messa
 
 			if (tree != null) {
 				try {
-					process(tree);
+					if (m_filter == null || m_filter.apply(tree)) {
+						process(tree);
+					}
 				} catch (Throwable e) {
 					handleException(e);
 				}
