19d9b33c8ed5be3f80908165eb4d19d8905b339d
chenyx <chenyx@ctrip.com>
2016-05-11 16:36:07 +0800

    event pipeline ready

--- cat-consumer/src/main/java/com/dianping/cat/consumer/build/Cat2ComponentsConfigurator.java
+++ cat-consumer/src/main/java/com/dianping/cat/consumer/build/Cat2ComponentsConfigurator.java
@@ -1,5 +1,6 @@
 package com.dianping.cat.consumer.build;
 
+import org.unidal.cat.plugin.event.EventPipeline;
 import org.unidal.cat.plugin.routing.DomainHashStrategy;
 import org.unidal.cat.plugin.routing.RoundRobinStrategy;
 import org.unidal.cat.plugin.transaction.TransactionPipeline;
@@ -15,6 +16,8 @@ public class Cat2ComponentsConfigurator extends AbstractResourceConfigurator {
         List<Component> all = new ArrayList<Component>();
 
         all.add(A(TransactionPipeline.class));
+        all.add(A(EventPipeline.class));
+
 
         all.add(A(DomainHashStrategy.class));
         all.add(A(RoundRobinStrategy.class));
---
+++ cat-consumer/src/main/java/org/unidal/cat/plugin/event/EventPipeline.java
@@ -0,0 +1,20 @@
+package org.unidal.cat.plugin.event;
+
+import org.unidal.cat.spi.analysis.pipeline.AbstractPipeline;
+import org.unidal.cat.spi.analysis.pipeline.Pipeline;
+import org.unidal.lookup.annotation.Named;
+
+import java.io.IOException;
+
+@Named(type = Pipeline.class, value = EventConstants.ID, instantiationStrategy = Named.PER_LOOKUP)
+public class EventPipeline extends AbstractPipeline {
+    @Override
+    protected void beforeCheckpoint() throws IOException {
+
+    }
+
+    @Override
+    protected void afterCheckpoint() {
+
+    }
+}
