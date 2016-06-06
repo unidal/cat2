b566c0ce6c48fbac7e5e9c593ba208d04ee7f0c6
yjhuang <yj.huang@Ctrip.com>
2016-06-06 16:53:33 +0800

    minor

--- cat-consumer/src/main/java/com/dianping/cat/consumer/build/Cat2ComponentsConfigurator.java
+++ cat-consumer/src/main/java/com/dianping/cat/consumer/build/Cat2ComponentsConfigurator.java
@@ -3,8 +3,10 @@ package com.dianping.cat.consumer.build;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.unidal.cat.config.internal.DBConfigManager;
 import org.unidal.cat.plugin.event.EventPipeline;
 import org.unidal.cat.plugin.transaction.TransactionAllReportMaker;
+import org.unidal.cat.plugin.transaction.TransactionConfigProvider;
 import org.unidal.cat.plugin.transaction.TransactionPipeline;
 import org.unidal.lookup.configuration.AbstractResourceConfigurator;
 import org.unidal.lookup.configuration.Component;
@@ -17,9 +19,12 @@ public class Cat2ComponentsConfigurator extends AbstractResourceConfigurator {
 		List<Component> all = new ArrayList<Component>();
 
 		all.add(A(TransactionPipeline.class));
-		all.add(A(EventPipeline.class));
-
 		all.add(A(TransactionAllReportMaker.class));
+
+        all.add(A(EventPipeline.class));
+
+        all.add(A(TransactionConfigProvider.class));
+        all.add(A(DBConfigManager.class));
 		all.add(A(ProjectService.class));
 
 		return all;
