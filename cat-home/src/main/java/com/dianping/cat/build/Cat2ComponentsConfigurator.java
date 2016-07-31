a3969cb2fd7c17ea604e50bb11a234a2de5b14d6
Frankie Wu <qmwu2000@gmail.com>
2016-07-31 16:42:36 +0800

    fix build issue

--- cat-home/src/main/java/com/dianping/cat/build/Cat2ComponentsConfigurator.java
+++ cat-home/src/main/java/com/dianping/cat/build/Cat2ComponentsConfigurator.java
@@ -3,6 +3,7 @@ package com.dianping.cat.build;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.unidal.cat.plugin.event.page.transform.AllReportDistributionBuilder;
 import org.unidal.lookup.configuration.AbstractResourceConfigurator;
 import org.unidal.lookup.configuration.Component;
 
@@ -10,6 +11,8 @@ public class Cat2ComponentsConfigurator extends AbstractResourceConfigurator {
 	@Override
 	public List<Component> defineComponents() {
 		List<Component> all = new ArrayList<Component>();
+
+		all.add(A(AllReportDistributionBuilder.class));
 
 		return all;
 	}
