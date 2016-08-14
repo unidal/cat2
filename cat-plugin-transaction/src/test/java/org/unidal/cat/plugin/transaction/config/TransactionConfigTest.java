59ebabe3ddae5e68a10e309c0181e3fb98d921d7
Frankie Wu <qmwu2000@gmail.com>
2016-08-14 17:15:38 +0800

    [cat-plugin-transaction] transaction config

---
+++ cat-plugin-transaction/src/test/java/org/unidal/cat/plugin/transaction/config/TransactionConfigTest.java
@@ -0,0 +1,19 @@
+package org.unidal.cat.plugin.transaction.config;
+
+import java.io.InputStream;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.unidal.cat.plugin.transaction.config.entity.TransactionConfigModel;
+import org.unidal.cat.plugin.transaction.config.transform.DefaultSaxParser;
+
+public class TransactionConfigTest {
+   @Test
+   public void test() throws Exception {
+      InputStream in = getClass().getResourceAsStream("transaction-config.xml");
+      TransactionConfigModel config = DefaultSaxParser.parse(in);
+
+      Assert.assertEquals(4, config.getLimits().size());
+   }
+}
