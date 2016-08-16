986c0dee0392a371617dc45791c2fb1e93a1dae8
Frankie Wu <qmwu2000@gmail.com>
2016-08-16 10:43:07 +0800

    [cat-plugin-transactions] add user document

--- cat-core-document/src/main/java/org/unidal/cat/core/document/spi/DocumentFeature.java
+++ cat-core-document/src/main/java/org/unidal/cat/core/document/spi/DocumentFeature.java
@@ -3,39 +3,53 @@ package org.unidal.cat.core.document.spi;
 import java.util.Set;
 
 public class DocumentFeature {
-	private String m_id;
+   private String m_id;
 
-	private String m_title;
+   private String m_title;
 
-	private String m_url;
+   private String m_url;
 
-	private Set<String> m_afterFeatures;
+   private Set<String> m_afterFeatures;
 
-	private Set<String> m_beforeFeatures;
+   private Set<String> m_beforeFeatures;
 
-	public DocumentFeature(String id, String title, String url) {
-		m_id = id;
-		m_title = title;
-		m_url = url;
-	}
+   public DocumentFeature(String id, String title, String url) {
+      m_id = id;
+      m_title = title;
+      m_url = url;
+   }
 
-	public String getId() {
-		return m_id;
-	}
+   @Override
+   public boolean equals(Object obj) {
+      if (obj instanceof DocumentFeature) {
+         return m_id.equals(((DocumentFeature) obj).m_id);
+      }
 
-	public String getTitle() {
-		return m_title;
-	}
+      return false;
+   }
 
-	public String getUrl() {
-		return m_url;
-	}
+   public Set<String> getAfterFeatures() {
+      return m_afterFeatures;
+   }
 
-	public Set<String> getAfterFeatures() {
-		return m_afterFeatures;
-	}
+   public Set<String> getBeforeFeatures() {
+      return m_beforeFeatures;
+   }
 
-	public Set<String> getBeforeFeatures() {
-		return m_beforeFeatures;
-	}
+   public String getId() {
+      return m_id;
+   }
+
+   public String getTitle() {
+      return m_title;
+   }
+
+   public String getUrl() {
+      return m_url;
+   }
+
+   @Override
+   public int hashCode() {
+      return m_id.hashCode();
+   }
 }
