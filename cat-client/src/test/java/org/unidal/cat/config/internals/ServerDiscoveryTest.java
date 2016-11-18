4914912914f283d828578c16a6b5d9ef05502262
Frankie Wu <qmwu2000@gmail.com>
2016-11-18 22:12:59 +0800

    [cat-client] add test cases

--- cat-client/src/test/java/org/unidal/cat/config/internals/ServerDiscoveryTest.java
+++ cat-client/src/test/java/org/unidal/cat/config/internals/ServerDiscoveryTest.java
@@ -1,46 +1,99 @@
 package org.unidal.cat.config.internals;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.unidal.helper.Files;
 import org.unidal.lookup.ComponentTestCase;
+import org.unidal.lookup.annotation.Named;
+
+import com.dianping.cat.configuration.client.entity.ClientConfig;
+import com.dianping.cat.configuration.client.entity.Server;
 
 public class ServerDiscoveryTest extends ComponentTestCase {
-   private static Map<String, String> MAP = new HashMap<String, String>();
-
    @Test
-   public void testFromEnvironmentVariable() throws Exception {
+   public void getByCatHost() throws Exception {
       defineComponent(ServerDiscovery.class, MockServerDiscovery.class);
 
-      MAP.clear();
-      MAP.put("env.CAT", "127.0.1.1,127.0.1.2");
+      System.setProperty("host", "true");
 
       try {
          ServerDiscovery d = lookup(ServerDiscovery.class);
          String servers = d.getMetaServers().toString();
 
-         Assert.assertEquals("[/127.0.1.1:2281, /127.0.1.2:2281]", servers);
+         Assert.assertEquals("[/127.0.0.1:2281]", servers);
       } finally {
+         System.getProperties().remove("host");
       }
    }
 
    @Test
-   public void testFromSystemProperties() {
-      System.setProperty("cat", "127.0.0.1,127.0.0.2:2281,127.0.0.3:2282");
+   public void getByDetection() throws Exception {
+      defineComponent(ServerDiscovery.class, MockServerDiscovery.class);
+
+      System.setProperty("detection", "true");
 
       try {
          ServerDiscovery d = lookup(ServerDiscovery.class);
          String servers = d.getMetaServers().toString();
 
-         Assert.assertEquals("[/127.0.0.1:2281, /127.0.0.2:2281, /127.0.0.3:2282]", servers);
+         Assert.assertEquals("[/127.0.0.1:2281, /127.0.0.2:2281, /127.0.0.3:2282, /127.0.0.4:2282]", servers);
+      } finally {
+         System.getProperties().remove("detection");
+      }
+   }
+
+   @Test
+   public void getFromClientXml() throws Exception {
+      defineComponent(Settings.class, MockSettings.class);
+      defineComponent(ServerDiscovery.class, MockServerDiscovery.class).req(Settings.class);
+
+      System.setProperty("client.xml", "true");
+
+      try {
+         ServerDiscovery d = lookup(ServerDiscovery.class);
+         String servers = d.getMetaServers().toString();
+
+         Assert.assertEquals("[/127.0.4.1:2281, /127.0.4.2:2282]", servers);
+      } finally {
+         System.getProperties().remove("client.xml");
+      }
+   }
+
+   @Test
+   public void getFromEnvironmentVariable() throws Exception {
+      defineComponent(ServerDiscovery.class, MockServerDiscovery.class);
+
+      System.setProperty("env", "127.0.2.1,127.0.2.2");
+
+      try {
+         ServerDiscovery d = lookup(ServerDiscovery.class);
+         String servers = d.getMetaServers().toString();
+
+         Assert.assertEquals("[/127.0.2.1:2281, /127.0.2.2:2281]", servers);
+      } finally {
+         System.getProperties().remove("env");
+      }
+   }
+
+   @Test
+   public void getFromSystemProperties() {
+      System.setProperty("cat", "127.0.3.1,127.0.3.2:2281,127.0.3.3:2282");
+
+      try {
+         ServerDiscovery d = lookup(ServerDiscovery.class);
+         String servers = d.getMetaServers().toString();
+
+         Assert.assertEquals("[/127.0.3.1:2281, /127.0.3.2:2281, /127.0.3.3:2282]", servers);
       } finally {
          System.getProperties().remove("cat");
       }
    }
 
+   @Named(type = ServerDiscovery.class)
    public static class MockServerDiscovery extends DefaultServerDiscovery {
       @Override
       protected int getDefaultServerHttpPort() {
@@ -48,13 +101,61 @@ public class ServerDiscoveryTest extends ComponentTestCase {
       }
 
       @Override
+      protected String getServersByCatHost() {
+         String host = System.getProperty("host");
+
+         if ("true".equals(host)) {
+            return "127.0.0.1";
+         } else {
+            return null;
+         }
+      }
+
+      @Override
+      protected String getServersByDetection() {
+         String detection = System.getProperty("detection");
+
+         if ("true".equals(detection)) {
+            return "127.0.0.1,127.0.0.2:2281,127.0.0.3:2282,127.0.0.4:2282";
+         } else {
+            return null;
+         }
+      }
+
+      @Override
       protected String getServersFromClientXml() {
-         return null;
+         String client = System.getProperty("client.xml");
+
+         if ("true".equals(client)) {
+            return super.getServersFromClientXml();
+         } else {
+            return null;
+         }
       }
 
       @Override
       protected String getServersFromEnvironmentVariable() {
-         return MAP.get("env.CAT");
+         return System.getProperty("env");
+      }
+   }
+
+   @Named(type = Settings.class)
+   public static class MockSettings extends DefaultSettings {
+      @Override
+      public File getClientXmlFile() {
+         try {
+            File file = File.createTempFile("test", "xml");
+            ClientConfig config = new ClientConfig();
+
+            config.addServer(new Server("127.0.4.1").setHttpPort(2281));
+            config.addServer(new Server("127.0.4.2").setHttpPort(2282));
+
+            Files.forIO().writeTo(file, config.toString());
+
+            return file;
+         } catch (IOException e) {
+            throw new RuntimeException(e);
+         }
       }
    }
 }
