b9a1c3b61a8c2b2f74a16a6f3b17d51c8bd7fa73
Frankie Wu <qmwu2000@gmail.com>
2016-12-02 16:12:20 +0800

    [cat-core-config] add system config database

--- cat-core-config/src/main/java/org/unidal/cat/core/config/build/ComponentsConfigurator.java
+++ cat-core-config/src/main/java/org/unidal/cat/core/config/build/ComponentsConfigurator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.unidal.cat.core.config.CatConfigModule;
+import org.unidal.cat.core.config.SystemConfigStoreGroup;
 import org.unidal.cat.core.config.service.DefaultContactorService;
 import org.unidal.cat.core.config.service.DefaultDomainGroupConfigService;
 import org.unidal.cat.core.config.service.DefaultDomainOrgConfigService;
@@ -22,6 +23,7 @@ public class ComponentsConfigurator extends AbstractResourceConfigurator {
       List<Component> all = new ArrayList<Component>();
 
       all.add(A(CatConfigModule.class));
+      all.add(A(SystemConfigStoreGroup.class));
 
       all.add(A(DefaultConfigStoreManager.class));
 
@@ -31,6 +33,7 @@ public class ComponentsConfigurator extends AbstractResourceConfigurator {
       all.add(A(DefaultDomainOrgConfigService.class));
       all.add(A(DefaultContactorService.class));
 
+      all.addAll(new CatDatabaseConfigurator().defineComponents());
       all.addAll(new WebComponentConfigurator().defineComponents());
 
       return all;
